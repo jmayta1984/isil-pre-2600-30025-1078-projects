be0dd4b5b2ac977549aa14fc0a99d6b0c0e50c50
jmayta1984 <jmayta@pucp.edu.pe>
2026-01-21 22:15:21 -0500

    chore: initial project setup

---
+++ EasyShop/EasyShop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EasyShop
+//
+//  Created by Jorge Mayta on 21/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EasyShop/EasyShop/EasyShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyShopApp.swift
+//  EasyShop
+//
+//  Created by Jorge Mayta on 21/01/26.
+//
+
+import SwiftUI
+
+@main
+struct EasyShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
